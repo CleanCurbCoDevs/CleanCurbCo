83b4753d4ac26e62403eca883cdcce236e235e80
Clean Curb Co <cleancurbco@stonebranchcapital.com>
2026-07-14 17:36:38 -0400

    Remove 'antialiased' class from layout
    Removed 'antialiased' class from the layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -104,7 +104,7 @@ export default function RootLayout({
     <html
       lang="en"
       data-scroll-behavior="smooth"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full`}
     >
       <body className="min-h-full flex flex-col">
         <ActionFeedbackProvider>
